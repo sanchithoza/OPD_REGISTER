e8a4172e0f4243f286382a50e94057c23a05048f
sanchithoza <sanchithoza@gmail.com>
2022-09-09 12:17:20 +0530

    fixed query for report

--- routes/opd.js
+++ routes/opd.js
@@ -12,26 +12,30 @@ async function routes(fastify, options) {
   });
   fastify.post("/getEntries", async (request, reply) => {
     let filter = {};
+    console.log(request.body.department,request.body.report_type);
     let query = `SELECT id,patient_name,DATE_FORMAT(transaction_date, '%Y-%m-%d') as created,DATE_FORMAT(transaction_date, '%Y-%m-%d') as transaction_date,entry_by,`;
     if (request.body.report_type == "lab") {
       query += `lab as amount`;
     } else if (request.body.report_type == "registration") {
-      query += `sum(registration + consultation) as amount`;
+      query += `(registration + consultation) as amount`;
     } else if (request.body.report_type == "medicine") {
-      query += `sum(dispensary +bio_chemic + mother_tincher) as amount`;
+      query += `(dispensary +bio_chemic + mother_tincher) as amount`;
     } else if (request.body.report_type == "other") {
-      query += `sum(minor_dressing + major_dressing + first_stiches + other_stiches + injection + ecg) as amount`;
+      query += `(minor_dressing + major_dressing + first_stiches + other_stiches + injection + ecg) as amount`;
     } else {
       query += `total as amount`;
     }
     query += ` from tbl_opd_register`;
     query += ` where transaction_date between '${request.body.from_date}' and '${request.body.to_date}'`;
     if (request.body.department != "all") {
-      query += ` and department = '${request.body.department}'`;
+      query += ` and department = '${request.body.department}' HAVING amount > 0`;
     }
+    console.log(query);
     await knex
       .raw(query)
       .then((result) => {
+       
+      // console.log(result[0]);
         reply.status(200).send(result[0]);
       })
       .catch((error) => {
